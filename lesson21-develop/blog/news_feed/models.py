26ce7d8decd7006b9e2881c55ffea92f5a820f13
Valery <valkirik1995@mail.ru>
2023-01-24 19:51:17 +0200

    add a file with a project

---
+++ lesson21-develop/blog/news_feed/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class DateTimeMixin(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True) #нужно чтобы дополнительно могли \
+    # наследоваться (чтобы эти два поля забрать себе в модель
+
+
+class Page(models.Model):
+    title = models.CharField(max_lenghth=150)
+    discription = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_privat = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"{self.pk} - {self.title}" #pk тот же IDб только pk в больших вариантах может сработать
+
+
+
+    class Meta:
+        vorbose_name = "Page"
+        vorbose_name_plural = "Pages"
+        
